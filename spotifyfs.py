3ab6a5e44bbebd1e7c27350dddd88932147be6b9
Amanda Huang <amandahuarng@gmail.com>
2020-05-01 00:26:41 +0800

    init running

--- spotifyfs.py
+++ spotifyfs.py
@@ -10,158 +10,41 @@ import sys
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
-
+scope = 'user-library-read'
 
 import os
 import sys
 import errno
 
 from fuse import FUSE, FuseOSError, Operations
+from Passthrough import Passthrough
 
+class Spfs(Passthrough):
 
-class Spfs(Operations):
-
-    # scope = 'user-library-read'
-
-    # if len(sys.argv) > 1:
-    #     username = sys.argv[1]
-    # else:
-    #     print("Usage: %s username" % (sys.argv[0],)) #my username is mkunz778 (because the app with the id and secret are in my name, it will only work for mine)
-    #     sys.exit()
-
-    # token = util.prompt_for_user_token(username, scope)
-
-    # if token:
-    #     sp = spotipy.Spotify(auth=token)
-    #     results = sp.current_user_saved_tracks()
-    #     for item in results['items']:
-    #         track = item['track']
-    #         print(track['name'] + ' - ' + track['artists'][0]['name'])
-    # else:
-    #     print("Can't get token for", username)
-
+    
     def __init__(self, root):
         self.root = root
-
-     # Helpers
-    # =======
-
-    def _full_path(self, partial):
-        if partial.startswith("/"):
-            partial = partial[1:]
-        path = os.path.join(self.root, partial)
-        return path
-
-    # Filesystem methods
-    # ==================
-
-    def access(self, path, mode):
-        full_path = self._full_path(path)
-        if not os.access(full_path, mode):
-            raise FuseOSError(errno.EACCES)
-
-    def chmod(self, path, mode):
-        full_path = self._full_path(path)
-        return os.chmod(full_path, mode)
-
-    def chown(self, path, uid, gid):
-        full_path = self._full_path(path)
-        return os.chown(full_path, uid, gid)
-
-    def getattr(self, path, fh=None):
-        full_path = self._full_path(path)
-        st = os.lstat(full_path)
-        return dict((key, getattr(st, key)) for key in ('st_atime', 'st_ctime',
-                     'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
-
-    def readdir(self, path, fh):
-        full_path = self._full_path(path)
-
-        dirents = ['.', '..']
-        if os.path.isdir(full_path):
-            dirents.extend(os.listdir(full_path))
-        for r in dirents:
-            yield r
-
-    def readlink(self, path):
-        pathname = os.readlink(self._full_path(path))
-        if pathname.startswith("/"):
-            # Path name is absolute, sanitize it.
-            return os.path.relpath(pathname, self.root)
+        if len(sys.argv) > 1:
+            username = sys.argv[1]
         else:
-            return pathname
+            print("Usage: %s username" % (sys.argv[0],)) #my username is mkunz778 (because the app with the id and secret are in my name, it will only work for mine)
+            sys.exit()
 
-    def mknod(self, path, mode, dev):
-        return os.mknod(self._full_path(path), mode, dev)
+        token = util.prompt_for_user_token(username, scope)
 
-    def rmdir(self, path):
-        full_path = self._full_path(path)
-        return os.rmdir(full_path)
-    
-    def mkdir(self, path, mode):
-        return os.mkdir(self._full_path(path), mode)
-
-    def statfs(self, path):
-        full_path = self._full_path(path)
-        stv = os.statvfs(full_path)
-        return dict((key, getattr(stv, key)) for key in ('f_bavail', 'f_bfree',
-            'f_blocks', 'f_bsize', 'f_favail', 'f_ffree', 'f_files', 'f_flag',
-            'f_frsize', 'f_namemax'))
-
-    def unlink(self, path):
-        return os.unlink(self._full_path(path))
-
-    def symlink(self, name, target):
-        return os.symlink(target, self._full_path(name))
-
-    def rename(self, old, new):
-        return os.rename(self._full_path(old), self._full_path(new))
-
-    def link(self, target, name):
-        return os.link(self._full_path(name), self._full_path(target))
-
-    def utimens(self, path, times=None):
-        return os.utime(self._full_path(path), times)
-
-    # File methods
-    # ============
-
-    def open(self, path, flags):
-        full_path = self._full_path(path)
-        return os.open(full_path, flags)
-
-    def create(self, path, mode, fi=None):
-        full_path = self._full_path(path)
-        return os.open(full_path, os.O_WRONLY | os.O_CREAT, mode)
-
-    def read(self, path, length, offset, fh):
-        os.lseek(fh, offset, os.SEEK_SET)
-        return os.read(fh, length)
-
-    def write(self, path, buf, offset, fh):
-        os.lseek(fh, offset, os.SEEK_SET)
-        return os.write(fh, buf)
-
-    def truncate(self, path, length, fh=None):
-        full_path = self._full_path(path)
-        with open(full_path, 'r+') as f:
-            f.truncate(length)
-
-    def flush(self, path, fh):
-        return os.fsync(fh)
-
-    def release(self, path, fh):
-        return os.close(fh)
-
-    def fsync(self, path, fdatasync, fh):
-        return self.flush(path, fh)
-
-
+        if token:
+            sp = spotipy.Spotify(auth=token)
+            results = sp.current_user_saved_tracks()
+            for item in results['items']:
+                track = item['track']
+                print(track['name'] + ' - ' + track['artists'][0]['name'])
+        else:
+            print("Can't get token for", username)
 
 
 def main(mountpoint, root):
     FUSE(Spfs(root), mountpoint, nothreads=True, foreground=True)
 
 if __name__ == '__main__':
-    main(sys.argv[2], sys.argv[1])
+    main(sys.argv[3], sys.argv[2])
 
